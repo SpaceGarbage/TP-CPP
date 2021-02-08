98fedef78827508a0d456c99fc579723cd2c2d5a
Gaëtan Chatelain <72392152+SpaceGarbage@users.noreply.github.com>
2021-02-08 17:56:30 +0100

    Aled

---
+++ include/Zombie.h
@@ -0,0 +1,19 @@
+#ifndef ZOMBIE_H
+#endif // ZOMBIE_H
+
+
+class Zombie
+{
+    public:
+    Zombie();
+    virtual~Zombie();
+
+    protected:
+
+    private:
+    int pv = 1;
+	int attack = 1;
+	string nom = "";
+};
+
+#endif//ZOMBIE_H
---
+++ src/Zombie.cpp
@@ -0,0 +1,16 @@
+#include "Zombie.h"
+
+Zombie::Zombie()
+{
+    pv = 1;
+	attack = 1;
+    nom = "Default";
+
+	cout<<"nom, pv, attack"<<endl;
+}
+
+
+Zombie::~Zombie()
+{
+    //dtor
+}
